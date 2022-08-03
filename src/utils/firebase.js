34aa686c9bfbadd2419aadbfb7b1f1e4e92eb42c
0xkiichiro <barisaytimur@gmail.com>
2022-08-03 15:01:37 +0300

    firebase boilerplate

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNtZDx12ivhJZKPbLsyxDnnmfsTU0NV3M",
+  authDomain: "fir-blog-app-127d9.firebaseapp.com",
+  projectId: "fir-blog-app-127d9",
+  storageBucket: "fir-blog-app-127d9.appspot.com",
+  messagingSenderId: "323095258221",
+  appId: "1:323095258221:web:9fb0a63391595d103b0b64",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
