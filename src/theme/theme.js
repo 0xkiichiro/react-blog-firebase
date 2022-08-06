000e58b3e972336fea4f3466a8997da1b7a4cf8f
0xkiichiro <barisaytimur@gmail.com>
2022-08-06 21:25:34 +0300

    todo

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,7 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
+  //TODO work on to change primary color theme instead!
   colors: {
     navBgColor: "hsl(252, 59%, 52%)",
     bgColor: "hsl(36, 99%, 55%)",
